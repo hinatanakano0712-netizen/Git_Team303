e82768e98c0637790957014574d28def01bc3d34
hinatanakano0712-netizen <nhs40106@nhs.hal.ac.jp>
2026-02-17 10:31:19 +0900

    test

---
+++ Assets/CScript/test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        cube.transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
